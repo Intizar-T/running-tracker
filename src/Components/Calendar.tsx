1cf800f57956563aec07484edb2bc87f46c3b561
Intizar Tashov <intizar.tashov22@gmail.com>
2023-07-28 02:00:17 +0300

    add mui icons and dummy calendar data

--- src/Components/Calendar.tsx
+++ src/Components/Calendar.tsx
@@ -7,21 +7,25 @@ import {
   TableRow,
   Typography,
 } from "@mui/material";
+import { useState } from "react";
+import ClearIcon from "@mui/icons-material/Clear";
 
 export default function Calendar() {
+  const [completedDay, setCompletedDay] = useState<number[]>([]);
   const currentMonth = new Date().getMonth() + 1;
   const currentYear = new Date().getFullYear();
   const totalDays = new Date(currentYear, currentMonth, 0).getDate();
   const weeks = Math.floor(totalDays / 7) + 1;
   const rows = [];
   for (let i = 0; i < weeks; i++) {
-    console.log(i);
     const columns = [];
     let columnNumbers = 7;
     if (7 * (i + 1) > totalDays) columnNumbers = totalDays - 7 * i;
     for (let j = 0; j < columnNumbers; j++) {
+      const today = i * 7 + j + 1;
       columns.push(
         <TableCell
+          key={today}
           align="center"
           sx={{
             borderRight: 1,
@@ -29,8 +33,20 @@ export default function Calendar() {
             height: "100px",
             minWidth: "100px",
           }}
+          onDoubleClick={() => {
+            if (completedDay.includes(today))
+              setCompletedDay(completedDay.filter((date) => date !== today));
+            else setCompletedDay([...completedDay, today]);
+          }}
         >
-          <Typography>{i * 7 + j + 1}</Typography>
+          {completedDay.includes(today) ? (
+            <ClearIcon fontSize="large" color="error" />
+          ) : (
+            <div>
+              <Typography>{today}</Typography>
+              <Typography>Event</Typography>
+            </div>
+          )}
         </TableCell>
       );
     }
@@ -40,8 +56,8 @@ export default function Calendar() {
     <TableContainer component={Paper}>
       <Table>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow>{row.map((column) => column)}</TableRow>
+          {rows.map((row, index) => (
+            <TableRow key={index}>{row.map((column) => column)}</TableRow>
           ))}
         </TableBody>
       </Table>
